b702b09bca625ebcecca0e5728b3210a7c54187f
KhoaPiterrr <ltk.gym@gmail.com>
2020-08-29 17:56:49 +0700

    setting tab-login 90%

--- client/src/containers/AuthPage/components/SignTabs/index.jsx
+++ client/src/containers/AuthPage/components/SignTabs/index.jsx
@@ -4,27 +4,38 @@ import LockOpenIcon from '@material-ui/icons/LockOpen';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import Logo from '../../../../assets/images/ltk.png';
 import { Link } from 'react-router-dom';
-const SignTabs = ({ children }) => {
+const SignTabs = ({ children, tabCurrent, onClickTabsSignIn, formTitle }) => {
+  const onClickTabs = (tab) => {
+    if (tab !== tabCurrent) {
+      onClickTabsSignIn(tab);
+    }
+  };
   return (
     <React.Fragment>
       <div className='register-login-forn'>
         <ul className='nav nav-tabs'>
           <li className='nav-item'>
-            <Link className='nav-link' to='/sign-in'>
+            <a
+              className={tabCurrent === true ? 'nav-link active' : 'nav-link'}
+              onClick={() => onClickTabs(true)}
+              href='javascript:void(0)'>
               <PowerSettingsNewIcon color='disabled' />
               <div className='ripple-container' />
-            </Link>
+            </a>
           </li>
           <li className='nav-item'>
-            <Link className='nav-link active' to='/sign-up'>
+            <a
+              className={tabCurrent === false ? 'nav-link active' : 'nav-link'}
+              onClick={() => onClickTabs(false)}
+              href='javascript:void(0)'>
               <LockOpenIcon color='disabled' />
               <div className='ripple-container' />
-            </Link>
+            </a>
           </li>
         </ul>
         <div className='content'>
           <div className='title'>
-            <img src={Logo} alt='' /> Login to your Account
+            <img src={Logo} alt='' /> {formTitle}
           </div>
           {children}
         </div>
@@ -33,6 +44,16 @@ const SignTabs = ({ children }) => {
   );
 };
 
-SignTabs.propTypes = {};
+SignTabs.propTypes = {
+  onClickTabsSignIn: PropTypes.func.isRequired,
+  tabCurrent: PropTypes.bool,
+  formTitle: PropTypes.string,
+};
+
+SignTabs.defaultProps = {
+  onClickTabsSignIn: null,
+  tabCurrent: true,
+  formTitle: 'Login to your Account',
+};
 
 export default SignTabs;
--- client/src/containers/AuthPage/index.jsx
+++ client/src/containers/AuthPage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import LoginPage from '../../components/LoginPage';
 import { useRouteMatch, Route } from 'react-router-dom';
@@ -9,14 +9,18 @@ import SignTabs from './components/SignTabs';
 import NotFound from '../../components/NotFound';
 
 const AuthPage = (props) => {
-  const match = useRouteMatch();
-  console.log({ match });
+  const [tabCurrent, settabCurrent] = useState(true);
 
   return (
     <React.Fragment>
       <LoginPage>
-        <SignTabs>
-          <SignIn />
+        <SignTabs
+          onClickTabsSignIn={settabCurrent}
+          tabCurrent={tabCurrent}
+          formTitle={
+            tabCurrent ? 'Login to your Account' : 'Register to Ltk-Social'
+          }>
+          {tabCurrent ? <SignIn /> : <SignUp />}
         </SignTabs>
       </LoginPage>
     </React.Fragment>
