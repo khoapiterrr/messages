7de120c1a6542b8a016584517c7eb35005d0bcff
KhoaPiterrr <ltk.gym@gmail.com>
2020-09-19 00:05:37 +0700

    design ui

--- client/src/components/MessagePage/index.jsx
+++ client/src/components/MessagePage/index.jsx
@@ -8,7 +8,16 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { getOffset } from '../../utils/common';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
-
+import UserGreenIMG from '../../assets/images/icon/user-green.png';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import CallIcon from '@material-ui/icons/Call';
+import LanguageIcon from '@material-ui/icons/Language';
+import { Col } from 'reactstrap';
+import VideoCallIMG from '../../assets/images/icon/video-camera-32.png';
+import InformationIMG from '../../assets/images/icon/information-32.png';
+import CallIMG from '../../assets/images/icon/phone-ringing-32px.png';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
 export const MessagePage = () => {
   const [style, setStyle] = useState({});
   useEffect(() => {
@@ -20,13 +29,8 @@ export const MessagePage = () => {
       <Header />
       <div className='message-home-page'>
         <Container fixed={false} maxWidth={false}>
-          <Grid
-            container
-            direction='row'
-            // spacing={3}
-            // alignItems='center'
-            justify='center'>
-            <Grid item xl={3} md={3} sm={4}>
+          <div className='row'>
+            <Col sm={5} md={4} xl={3}>
               <div className='conversion-wrapper'>
                 <div className='title'>
                   <h2 className='h2'>Conversion</h2>
@@ -67,7 +71,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -86,7 +90,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -105,7 +109,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -124,7 +128,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -143,7 +147,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -162,7 +166,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -181,7 +185,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -200,7 +204,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -219,7 +223,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -238,7 +242,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -257,7 +261,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -276,7 +280,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -295,7 +299,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -314,7 +318,7 @@ export const MessagePage = () => {
                         className='avatar'
                       />
                       <div className='info-chat-item'>
-                        <p className='user-name'>Trong Khoa</p>
+                        <p className='h5 user-name'>Trong Khoa</p>
                         <p className='last-chat'>
                           Nice-try bro hihi. &nbsp; <span>●6.50 pm</span>
                         </p>
@@ -329,19 +333,97 @@ export const MessagePage = () => {
                   </div>
                 </div>
               </div>
-            </Grid>
-            <Grid item xl={6} md={6} sm='auto'></Grid>
-            <Grid item xl={3} md={3} sm={3}>
-              <div className='user-info-wrapper'>
+            </Col>
+            <div className='col'>
+              <div className='content-inbox'>
+                <div className='mes-card content-inbox__header '>
+                  <Avatar
+                    alt='Trong khoa'
+                    src='https://scontent.fhan3-3.fna.fbcdn.net/v/t1.0-9/30709828_2032828676992328_6094577782937878528_o.jpg?_nc_cat=101&_nc_sid=09cbfe&_nc_ohc=A-9YBVNn1gAAX8K3md1&_nc_ht=scontent.fhan3-3.fna&oh=79b14e3b4ea1d8bda4561951ae855561&oe=5F89370E'
+                    className='avatar'
+                  />
+                  <div className='info'>
+                    <p className='h4'>KhoaPiterrr</p>
+                    <p className='status'>Online</p>
+                  </div>
+                  <div className='inbox-action'>
+                    <img src={VideoCallIMG} className='mx-3' alt='' />
+                    <img src={CallIMG} alt='' className='mx-3' />
+                    <img src={InformationIMG} className='mx-3' alt='' />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Col sm={5} md={3}>
+              <div className='user-info-wrapper mb-3'>
                 <div className='title'>
                   <h2 className='h2'>User Info</h2>
                   <span>
                     <HighlightOffIcon />
                   </span>
                 </div>
+                <div className='mes-card user-intro mb-3'>
+                  <div className='user-intro__content'>
+                    <Avatar
+                      alt='Trong khoa'
+                      src='https://scontent.fhan3-3.fna.fbcdn.net/v/t1.0-9/30709828_2032828676992328_6094577782937878528_o.jpg?_nc_cat=101&_nc_sid=09cbfe&_nc_ohc=A-9YBVNn1gAAX8K3md1&_nc_ht=scontent.fhan3-3.fna&oh=79b14e3b4ea1d8bda4561951ae855561&oe=5F89370E'
+                      className='avatar'
+                    />
+                    <p className='h4 username mt-3 font-weight-bold'>
+                      Trong Khoa
+                    </p>
+                    <p className='status'>last seen today at 23:06 pm</p>
+                  </div>
+                </div>
+                <div className='mes-card contact-info mb-3'>
+                  <div className='contact-info__header'>
+                    <img src={UserGreenIMG} alt='' />
+                    <span className='h5'>Contact Information</span>
+                    <div className='more'>
+                      <MoreHorizIcon />
+                    </div>
+                  </div>
+                  <div className='contact-info__body'>
+                    <p className='info-item'>
+                      <MailOutlineIcon className='icon' />{' '}
+                      <span>ltk.gym@gmail.com</span>
+                    </p>
+                    <p className='info-item'>
+                      <CallIcon className='icon' /> <span>+84 393 837 623</span>
+                    </p>
+                    <p className='info-item'>
+                      <LanguageIcon className='icon' />{' '}
+                      <span>www.google.com</span>
+                    </p>
+                  </div>
+                </div>
+                <div className='mes-card add-tag mb-3'>
+                  <LocalOfferIcon className='user-info__icon' />{' '}
+                  <span>Add Tag</span>
+                </div>
+                <div className='mes-card group-common mb-3'>
+                  <LocalOfferIcon className='user-info__icon' />{' '}
+                  <span>Group in common (3)</span>
+                </div>
+                <div className='mes-card share-file mb-3'>
+                  <LocalOfferIcon className='user-info__icon' />{' '}
+                  <span>Share</span> Files
+                </div>
+                <div className='mes-card block-user mb-3'>
+                  <LocalOfferIcon className='user-info__icon' />{' '}
+                  <span>Block User</span>
+                </div>
+                <div className='mes-card report-user mb-3'>
+                  <LocalOfferIcon className='user-info__icon' />{' '}
+                  <span>Report User</span>
+                </div>
+                <div className='mes-card delete-chat mb-3'>
+                  <LocalOfferIcon className='user-info__icon' />{' '}
+                  <span> Delete Chat</span>
+                </div>
               </div>
-            </Grid>
-          </Grid>
+            </Col>
+          </div>
         </Container>
       </div>
     </div>
